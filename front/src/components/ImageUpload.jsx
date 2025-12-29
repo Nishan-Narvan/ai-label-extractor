3039bb2da7fc563210a6ec1d82abb877d4cfe560
Nishan <121342495+Nishan-Narvan@users.noreply.github.com>
2025-12-29 17:27:49 +0530

    Fix API URL in ImageUpload component

--- front/src/components/ImageUpload.jsx
+++ front/src/components/ImageUpload.jsx
@@ -62,7 +62,7 @@ function ImageUpload() {
 
      const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://ai-label-extractor-back.vercel.app";
+  "https:/ai-label-extractor-back.vercel.app";
 
 
       const response = await fetch(`${API_URL}/api/extract`, {
